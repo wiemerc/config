42d0049df80c62f69f1af8b398a3d68fc7b33f46
Constantin Wiemer <constantin.wiemer@gmx.de>
2026-01-07 11:58:41 +0100

    Check if a host name is specified

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -o errexit
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 <host name, e. g. home1>"
+    exit 1
+fi
 
 echo "Installing git and pip..."
 sudo dnf install -yq git python3-pip
@@ -13,4 +17,4 @@ git clone https://github.com/wiemerc/config.git
 
 echo "Running Ansible playbook..."
 cd config/ansible
-~/.local/bin/ansible-playbook -i inventory.ini --limit mvhs1 setup.yml
+~/.local/bin/ansible-playbook -i inventory.ini --limit $1 setup.yml
